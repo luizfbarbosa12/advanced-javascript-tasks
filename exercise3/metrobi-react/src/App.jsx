9798acf484d868eb72c55058e2c7b5a5103decb4
luizfbarbosa12 <l.nandoferbarbosa@gmail.com>
2023-03-25 20:35:55 -0300

    exercise 3 done

--- exercise3/metrobi-react/src/App.jsx
+++ exercise3/metrobi-react/src/App.jsx
@@ -1,20 +1,45 @@
-import { Container, Header, Hero, MainContent, Sidebar, ExtraContent } from "./app.styles";
+import {
+  Container,
+  Header,
+  Hero,
+  Main,
+  Sidebar,
+  ExtraContent,
+  InnerContent,
+  MainContent,
+  Footer,
+  RelatedPosts,
+  RelatedImages,
+  LowerContent,
+  LeftGap,
+  RightGap
+} from "./app.styles";
+import { GlobalStyle } from "./GlobalStyles/GlobalStyle";
 function App() {
   return (
-    <Container>
-      <Header>Header</Header>
-      <div>
-        <div>
-          <Hero>HERO</Hero>
-          <Sidebar>SIDEBAR</Sidebar>
-          <MainContent>MAIN CONTENT</MainContent>
-          <ExtraContent>EXTRA CONTENT</ExtraContent>
-        </div>
-        <div></div>
-        <div></div>
-      </div>
-      <footer></footer>
-    </Container>
+    <>
+      <GlobalStyle />
+      <Container>
+        <Header>Header</Header>
+        <InnerContent>
+          <MainContent>
+            <LeftGap>
+              <Hero>HERO</Hero>
+              <Sidebar>SIDEBAR</Sidebar>
+            </LeftGap>
+            <RightGap>
+              <Main>MAIN CONTENT</Main>
+              <ExtraContent>EXTRA CONTENT</ExtraContent>
+            </RightGap>
+          </MainContent>
+        </InnerContent>
+        <LowerContent>
+          <RelatedImages>Related Images</RelatedImages>
+          <RelatedPosts>Related Posts</RelatedPosts>
+        </LowerContent>
+        <Footer>Footer</Footer>
+      </Container>
+    </>
   );
 }
 
--- exercise3/metrobi-react/src/app.styles.js
+++ exercise3/metrobi-react/src/app.styles.js
@@ -3,35 +3,141 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  & > * {
-    border: 1px solid red;
+  align-items: center;
+  gap: 0.5rem;
+`;
+
+export const LeftGap = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+`;
+export const RightGap = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+`;
+export const InnerContent = styled.div`
+  display: flex;
+  gap: 0.5rem;
+`;
+export const MainContent = styled.div`
+  display: flex;
+  gap: 0.5rem;
+
+  @media screen and (max-width: 51.875rem) {
+    display: block;
+  }
+`;
+export const Header = styled.header`
+  height: 4rem;
+  width: 51.3rem;
+  background-color: #54d5eb;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-transform: uppercase;
+
+  @media screen and (max-width: 51.875rem) {
+    width: 30.625rem;
   }
 `;
 
-export const Header = styled.header`
-  height: 5vh;
-  width: 100%;
-`;
-
 export const Hero = styled.article`
-  height: 18rem;
-  width: 18rem;
-  background-color: violet;
+  height: 17.125rem;
+  width: 20.25rem;
+  background-color: #d6c9e4;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-transform: uppercase;
+
+  @media screen and (max-width: 51.875rem) {
+    width: 100%;
+  }
 `;
 
-export const MainContent = styled.main`
-  width: 18rem;
-  height: 18rem;
-  background-color: goldenrod;
+export const Main = styled.main`
+  width: 30.625rem;
+  height: 27.5rem;
+  background-color: #f5c631;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-transform: uppercase;
 `;
 
 export const Sidebar = styled.aside`
-  height: 18rem;
-  width: 18rem;
-  background-color: greenyellow;
+  height: 22.8rem;
+  width: 20.25rem;
+  background-color: #9fc363;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-transform: uppercase;
+  @media screen and (max-width: 51.875rem) {
+    width: 100%;
+    margin-bottom: 0.5rem;
+  }
 `;
 export const ExtraContent = styled.aside`
-  height: 18rem;
-  width: 18rem;
-  background-color: lightgrey;
+  height: 12.375rem;
+  width: 30.625rem;
+  background-color: #898989;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-transform: uppercase;
+`;
+
+export const RelatedImages = styled.div`
+  background-color: #2bb673;
+  width: 35.875rem;
+  height: 9.75rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-transform: uppercase;
+
+  @media screen and (max-width: 51.875rem) {
+    width: 30.625rem;
+  }
+`;
+
+export const RelatedPosts = styled.div`
+  background-color: #f3ccde;
+  width: 15rem;
+  height: 9.75rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-transform: uppercase;
+
+  @media screen and (max-width: 51.875rem) {
+    width: 30.625rem;
+  }
+`;
+
+export const LowerContent = styled.div`
+  display: flex;
+  gap: 0.5rem;
+
+  @media screen and (max-width: 51.875rem) {
+    display: flex;
+    max-width: 30.625rem;
+    flex-direction: column;
+  }
+`;
+export const Footer = styled.footer`
+  background-color: #ffa500;
+  width: 51.375rem;
+  height: 4rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-transform: uppercase;
+
+  @media screen and (max-width: 51.875rem) {
+    width: 30.625rem;
+  }
 `;
